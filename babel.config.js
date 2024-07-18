f6868de1781b45b3aef018c078c5fc47d08e413c
fgarcia <gfabian757@gmail.com>
2024-07-18 15:22:50 -0600

    build: config absolute imports

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,23 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+     plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            assets: './assets',
+            components: './src/components',
+            screens: './src/screens',
+            hooks: './src/hooks',
+            hoc: './src/hoc',
+            config: './src/config',
+            types: './src/types',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
